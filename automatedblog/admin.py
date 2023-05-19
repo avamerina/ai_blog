a34d2efcde3d5518a56a26a81127da933e009598
avamerina <avamerina@gmail.com>
2023-05-19 23:29:15 +0600

    admin setup

--- automatedblog/admin.py
+++ automatedblog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Topic
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('date', 'topic', 'body', 'picture')
+    list_filter = ('date', 'topic')
+    search_fields = ('date', 'topic')
